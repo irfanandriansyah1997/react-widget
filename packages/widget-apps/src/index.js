10af67ef569d524a9660ea7b861ae865145e3c9c
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2021-12-01 21:16:52 +0700

    feat(setup): initial project

---
+++ packages/widget-apps/src/index.js
@@ -0,0 +1,1 @@
+console.debug('hi from widget');
---
+++ packages/widget-apps/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  //Where files should be sent once they are bundled
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
+  //webpack 5 comes with devServer which loads in development mode
+  devServer: {
+    port: 9000,
+    watchContentBase: true
+  },
+  //Rules of how webpack will take our files, complie & bundle them for the browser 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()],
+}
